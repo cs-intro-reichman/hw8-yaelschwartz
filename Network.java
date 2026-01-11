e029257cc9aec9babb3289578ab0d144b1d6b134
Yael Schwartz <yaelrebecca.schwartz@post.runi.ac.il>
2026-01-11 22:58:13 +0200

    Fix toString formatting in Network class

--- Network.java
+++ Network.java
@@ -133,10 +133,11 @@ public class Network {
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
-    public String toString() {
-        String ans = "Network:ֿֿ\n";
+   public String toString() {
+        String ans = "Network:"; 
+
         for (int i = 0; i < userCount; i++) {
-            ans = ans + users[i].toString() + "\n";
+            ans = ans + "\n" + users[i].toString();
         }
         return ans;
     }
